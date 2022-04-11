8b0e7cec636c07a40d88b00e37416b14138d70cd
jpratts <76081318+jpratts@users.noreply.github.com>
2022-04-11 15:25:58 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+# Uses tflwrapper, a Python wrapper for the Transport for London API, to pull data about bike point use around Greater London. 
+from tflwrapper import bikePoint, crowding
+import pandas as pd
+
+# Sign-up to TFL Open Data service for API details
+app_key = "reference_API_key_storage_here"
+
+# Retrieves crowding level at London Bridge Station using station code. Saves this to a DataFrame
+crwd = crowding(app_key)
+crowd_data = crwd.getLiveByNaptan("940GZZLULNB")
+crowd_pd = pd.DataFrame([crowd_data])
+
+# Retrieves details from all bike points around Greater London. They require cleaning given that its a multi-nested list
+bkpt = bikePoint(app_key)
+bike_data = bkpt.getAll()
+
+# Retrieve info of interest and append to list
+def bike_clean(data):
+    bike_list = []
+    n = 0
+    for x in data:
+        bikes_list = bike_list.append([
+        bike_data[n]["id"],
+        bike_data[n]["commonName"],
+        bike_data[n]["placeType"],
+        bike_data[n]["lat"],
+        bike_data[n]["lon"],
+        bike_data[n]["additionalProperties"][6]["value"],
+        bike_data[n]["additionalProperties"][7]["value"],
+        bike_data[n]["additionalProperties"][8]["value"],
+        bike_data[n]["additionalProperties"][0]["modified"]
+        ])
+        n += 1
+    return(bike_list)
+
+# Call function to retrieve data, then turn it into a DF and rename columns
+bike_pd = pd.DataFrame(bike_clean(bike_data))
+bike_pd = bike_pd.rename(columns={
+    0:"bikepoint_number",
+    1:"location_name",
+    2:"place_type",
+    3:"lat",
+    4:"lon",
+    5:"nb_bikes",
+    6:"empty_docks",
+    7:"nb_docks",
+    8:"dat_modified"})
+
+
