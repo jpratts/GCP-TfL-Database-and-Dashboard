45040b962e18ed95f6387bc6b8b94301a30e8a41
jpratts <76081318+jpratts@users.noreply.github.com>
2022-04-11 16:46:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ import pandas as pd
 import pandas_gbq
 import datetime
 
+def pull_data(event, context):
+    #Credentials for BigQuery Access
+    credentials_path = 'gcp-service-account-auth-key.json'
+    credentials = service_account.Credentials.from_service_account_file(credentials_path)
+    project_id = "PROJECT_ID"
 
-# Sign-up to TFL Open Data service for API details
-app_key = "reference_API_key_storage_here"
+    # Sign-up to TFL Open Data service for API details
+    app_key = "reference_API_key_storage_here"
 
-# Retrieves crowding level at London Bridge Station using station code. Saves this to a DataFrame
-crwd = crowding(app_key)
-crowd_data = crwd.getLiveByNaptan("940GZZLULNB")
-crowd_pd = pd.DataFrame([crowd_data])
+    # Retrieves crowding level at London Bridge Station using station code. Saves this to a DataFrame
+    crwd = crowding(app_key)
+    crowd_data = crwd.getLiveByNaptan("940GZZLULNB")
+    crowd_pd = pd.DataFrame([crowd_data])
 
-# Retrieves details from all bike points around Greater London. They require cleaning given that its a multi-nested list
-bkpt = bikePoint(app_key)
-bike_data = bkpt.getAll()
+    # Retrieves details from all bike points around Greater London. They require cleaning given that its a multi-nested list
+    bkpt = bikePoint(app_key)
+    bike_data = bkpt.getAll()
 
-# Retrieve info of interest and append to list
-def bike_clean(data):
+    # Clean up data from the bike pull
     bike_list = []
     n = 0
-    for x in data:
+    for x in bike_data:
         bikes_list = bike_list.append([
         bike_data[n]["id"],
         bike_data[n]["commonName"],
@@ -34,39 +38,31 @@ def bike_clean(data):
         bike_data[n]["additionalProperties"][8]["value"],
         bike_data[n]["additionalProperties"][0]["modified"]
         ])
-        n += 1
-    return(bike_list)
+        n += 1)
 
-# Call function to retrieve data, then turn it into a DF and rename columns
-bike_pd = pd.DataFrame(bike_clean(bike_data))
-bike_pd = bike_pd.rename(columns={
-    0:"bikepoint_number",
-    1:"location_name",
-    2:"place_type",
-    3:"lat",
-    4:"lon",
-    5:"nb_bikes",
-    6:"empty_docks",
-    7:"nb_docks",
-    8:"dat_modified"})
+      bike_pd = pd.DataFrame(bike_data)
+      bike_pd = bike_pd.rename(columns={
+        0:"bikepoint_number",
+        1:"location_name",
+        2:"place_type",
+        3:"lat",
+        4:"lon",
+        5:"nb_bikes",
+        6:"empty_docks",
+        7:"nb_docks",
+        8:"dat_modified"})
 
-# Pass in GCP credentials
-credentials = ...  # From google-auth or pydata-google-auth library
+    # Project id used in GCP
+    project_id = "tfl_project"
 
-# Update the in-memory credentials cache
-pandas_gbq.context.credentials = credentials
+    # Table which bike data will be appended to
+    bike_table = "tfl_data.bikes"
 
-# Project id used in GCP
-project_id = "tfl_project"
+    # Table which crowd data will be appended to
+    crowd_table = "tfl_data.crowd"
 
-# Table which bike data will be appended to
-bike_table = "tfl_data.bikes"
+    # Uses gbq to pass data into bigquery
+    pandas_gbq.to_gbq(bike_pd, bike_table, project_id=project_id, credentials=credentials, if_exists="append")
 
-# Table which crowd data will be appended to
-crowd_table = "tfl_data.crowd"
-
-# Uses gbq to pass data into bigquery
-pandas_gbq.to_gbq(bike_pd, bike_table, project_id=project_id, if_exists="append")
-
-# Uses gbq to pass data into bigquery
-pandas_gbq.to_gbq(crowd_pd, crowd_table, project_id=project_id, if_exists="append")
+    # Uses gbq to pass data into bigquery
+    pandas_gbq.to_gbq(crowd_pd, crowd_table, project_id=project_id, credentials=credentials, if_exists="append")
