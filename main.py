f312a669d95c3e73c20ce6f79f99fff3c1a21e0e
jpratts <76081318+jpratts@users.noreply.github.com>
2022-04-11 20:07:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # Uses tflwrapper, a Python wrapper for the Transport for London API, to pull data about bike point use around Greater London. 
 from tflwrapper import bikePoint, crowding
 from google.cloud import bigquery
+from google.oauth2 import service_account
 import pandas as pd
 import pandas_gbq
 import datetime
 
 def pull_data(event, context):
     #Credentials for BigQuery Access
-    credentials_path = 'gcp-service-account-auth-key.json'
+    credentials_path = ~
     credentials = service_account.Credentials.from_service_account_file(credentials_path)
-    project_id = "PROJECT_ID"
 
     # Sign-up to TFL Open Data service for API details
-    app_key = "reference_API_key_storage_here"
+    app_key = ~
 
     # Retrieves crowding level at London Bridge Station using station code. Saves this to a DataFrame
     crwd = crowding(app_key)
@@ -38,31 +38,32 @@ def pull_data(event, context):
         bike_data[n]["additionalProperties"][8]["value"],
         bike_data[n]["additionalProperties"][0]["modified"]
         ])
-        n += 1)
+        n = n + 1
 
-      bike_pd = pd.DataFrame(bike_list)
-      bike_pd = bike_pd.rename(columns={
-        0:"bikepoint_number",
-        1:"location_name",
-        2:"place_type",
-        3:"lat",
-        4:"lon",
-        5:"nb_bikes",
-        6:"empty_docks",
-        7:"nb_docks",
-        8:"dat_modified"})
+    bike_pd = pd.DataFrame(bike_list)
+    bike_pd = bike_pd.rename(columns={
+    0:"bikepoint_number",
+    1:"location_name",
+    2:"place_type",
+    3:"lat",
+    4:"lon",
+    5:"nb_bikes",
+    6:"empty_docks",
+    7:"nb_docks",
+    8:"dat_modified"})
 
     # Project id used in GCP
-    project_id = "tfl_project"
+    project_id = ~
 
     # Table which bike data will be appended to
-    bike_table = "tfl_data.bikes"
+    bike_table = "tfl_data.bike_table"
 
     # Table which crowd data will be appended to
-    crowd_table = "tfl_data.crowd"
+    crowd_table = "tfl_data.crowd_table"
 
     # Uses gbq to pass data into bigquery
     pandas_gbq.to_gbq(bike_pd, bike_table, project_id=project_id, credentials=credentials, if_exists="append")
-
+    
     # Uses gbq to pass data into bigquery
     pandas_gbq.to_gbq(crowd_pd, crowd_table, project_id=project_id, credentials=credentials, if_exists="append")
+
